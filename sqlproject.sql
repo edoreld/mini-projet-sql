9bdf07924fc134845936e54e5c0b30027f181beb
edoreld <edoreld@gmail.com>
2017-01-18 14:32:53 +0100

    more info

--- sqlproject.sql
+++ sqlproject.sql
@@ -1,12 +1,47 @@
+#####
+#
+# Contexte: Passage au relationnel du mini-projet Bases de données relationnel Licence MIDO 3ème année par alternance
+
+# Car on ne peut pas identifier une rélation uniquement par les trois attribus de la classe Match sur le diagramme UML, on ajoute
+# un attribut MatchID qui sera notre clé primaire. 
+
 CREATE TABLE Match 
 (
-	MatchID	SERIAL,
+	MatchID		serial UNIQUE,
+	date_match	date
 	heure_debut	time,
 	heure_fin	time,
-	CONSTRAINT pk_matchid PRIMARY KEY MatchID
+	CONSTRAINT 	pk_matchid PRIMARY KEY MatchID
 )
 
-CREATE
+# La table Equipe avec les attributs initiales et la clé primaire 'nom' identifiant un equipe
+
+CREATE TABLE Equipe 
+(
+	nom VARCHAR,
+	numJoueurs SMALLINT,
+	CONSTRAINT pk_nom PRIMARY KEY nom
+)
+
+# How to deal with inheritance ?
+#  - http://stackoverflow.com/questions/3579079/how-can-you-represent-inheritance-in-a-database
+#  
+CREATE TABLE Joeur 
+(
+	numero 			SMALLINT,
+	nom 			VARCHAR,
+	prenom			VARCHAR,
+	datenaissance	date
+)
+
+# La table but
+CREATE TABLE But 
+(
+	heure 			time,
+	match_id		integer,
+	numero_joueur	
+)
+
 
 
 
